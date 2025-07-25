4640f5ff605876d0d2fbfda43efac0865a97a449
Rohit Singh <rohitsingh@192.168.1.5>
2025-07-25 15:52:34 +0530

    Empty project

---
+++ android/app/src/main/kotlin/com/example/tdd_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tdd_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("First Screen")),
+      body: Center(child: Text("Hello World")),
+    );
+  }
+}
